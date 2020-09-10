3b0aa92a828318df234f1388409576ce24b863b3
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-09-10 19:15:01 +0200

    Extract encrypting single letter to method

--- src/main/java/caesarEncryption/Main.java
+++ src/main/java/caesarEncryption/Main.java
@@ -71,19 +71,23 @@ class Caesar {
     private static String encryptMessage(String input) {
         StringBuilder output = new StringBuilder();
         for (int i = 0; i < input.length(); i++) {
-            char letter = input.charAt(i);
-            // encrypt a single letter
-            if ('A' <= letter && letter <= 'Z') {
-                if (letter + 3 <= 'Z') {
-                    output.append((char)(letter + 3));
-                } else {
-                    output.append((char)(letter + 3 - 26));
-                }
-            } else {
-                output.append(letter);
-            }
+            output.append(encryptLetter(input.charAt(i)));
         }
         return output.toString();
+    }
+
+    private static char encryptLetter(char letter) {
+        char character;
+        if ('A' <= letter && letter <= 'Z') {
+            if (letter + 3 <= 'Z') {
+                character = (char)(letter + 3);
+            } else {
+                character = (char)(letter + 3 - 26);
+            }
+        } else {
+            character = letter;
+        }
+        return character;
     }
 
     private static String readPlainTextMessageToDecrypt(Scanner in) {
